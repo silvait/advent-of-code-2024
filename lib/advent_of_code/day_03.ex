8fac31433ce4d4f4c14422db388788a7f006f4bb
Paulo Silva <github@p.silvahq.com>
2024-12-03 11:54:27 -0500

    Add day3 solution

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,58 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  defp translate_operation([operation]) do
+    case operation do
+      "do()" -> :enable
+      "don't()" -> :disable
+    end
   end
 
-  def part2(_args) do
+  defp translate_operation([_, num1, num2]) do
+    {String.to_integer(num1), String.to_integer(num2)}
+  end
+
+  defp parse_operations(str) do
+    regex = ~r/mul\((\d+),(\d+)\)/
+
+    Regex.scan(regex, str)
+    |> Enum.map(fn [_, num1, num2] ->
+      {String.to_integer(num1), String.to_integer(num2)}
+    end)
+  end
+
+  defp parse_operations2(str) do
+    regex = ~r/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/
+
+    Regex.scan(regex, str)
+    |> Enum.map(&translate_operation/1)
+  end
+
+  defp perform_operations(operations) do
+    Enum.map(operations, fn {x, y} -> x * y end)
+  end
+
+  defp perform_operations2(operations) do
+    {_, result} = Enum.reduce(operations, { true, 0 }, fn token, {status, result} ->
+      case [token, status] do
+        [:disable,_] -> {false, result}
+        [:enable,_] -> {true, result}
+        [_, false] -> {status, result}
+        [{x,y}, true] -> {status, (result + (x * y))}
+      end
+    end)
+
+    result
+  end
+
+  def part1(input) do
+    input
+    |> parse_operations()
+    |> perform_operations()
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> parse_operations2()
+    |> perform_operations2()
   end
 end
