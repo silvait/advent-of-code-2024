cf69368e109037ffb8735b8f0b8d992fae254fe5
Paulo Silva <github@p.silvahq.com>
2024-12-04 14:26:15 -0500

    refactor

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -21,52 +21,30 @@ defmodule AdventOfCode.Day04 do
   end
 
   defp create_search_paths(lines) do
-    lines ++ columns(lines) ++ diagonals(lines, :right) ++ diagonals(lines, :left)
+    lines ++ columns(lines) ++ diagonals(lines) ++ diagonals(reversed(lines))
   end
 
-  def diagonals(lines, direction) do
+  def diagonals(lines) do
     total_columns = String.length(Enum.at(lines, 0))
+    first_row = MapSet.new(0..(total_columns - 1), &{0, &1})
+
     total_rows = Enum.count(lines)
+    first_column = MapSet.new(0..(total_rows - 1), &{&1, 0})
 
-    [inc, start_col] =
-      case direction do
-        :right -> [{1, 1}, 0]
-        :left -> [{1, -1}, total_columns - 1]
-      end
+    start_list = MapSet.union(first_row, first_column)
 
-    first =
-      Enum.map(
-        0..(total_columns - 1),
-        &walk_diagonal(lines, {0, &1}, total_rows, total_columns, inc)
-      )
-
-    second =
-      Enum.map(
-        1..(total_rows - 1),
-        &walk_diagonal(lines, {&1, start_col}, total_rows, total_columns, inc)
-      )
-
-    first ++ second
+    Enum.map(start_list, fn {row, col} ->
+      walk_diagonal(Enum.drop(lines, row), col, total_columns)
+    end)
   end
 
-  defp walk_diagonal(_, {row, col}, total_rows, total_columns, _)
-       when row < 0 or col < 0 or row >= total_rows or col >= total_columns do
-    ""
+  defp walk_diagonal([current_line | lines], col, total_columns) when col < total_columns do
+    String.at(current_line, col) <> walk_diagonal(lines, col + 1, total_columns)
   end
 
-  defp walk_diagonal(lines, {row, col}, total_rows, total_columns, {row_inc, col_inc}) do
-    current_string = Enum.at(lines, row)
-    current_letter = String.at(current_string, col)
+  defp walk_diagonal(_, _, _), do: ""
 
-    current_letter <>
-      walk_diagonal(
-        lines,
-        {row + row_inc, col + col_inc},
-        total_rows,
-        total_columns,
-        {row_inc, col_inc}
-      )
-  end
+  defp reversed(lines), do: Enum.map(lines, &String.reverse/1)
 
   defp columns(lines) do
     lines
@@ -75,9 +53,7 @@ defmodule AdventOfCode.Day04 do
     |> Enum.map(&(Tuple.to_list(&1) |> Enum.join("")))
   end
 
-  defp count_matches(line, pattern) do
-    Regex.scan(pattern, line) |> Enum.count()
-  end
+  defp count_matches(line, pattern), do: Regex.scan(pattern, line) |> Enum.count()
 
   def part2(_args) do
   end
