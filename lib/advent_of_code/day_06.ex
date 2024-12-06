1b0629c3efd91f2590f0faf07ad36076a5c0cd5b
Paulo Silva <github@p.silvahq.com>
2024-12-06 11:57:11 -0500

    renames

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -4,33 +4,33 @@ defmodule AdventOfCode.Day06 do
   @south {0, 1}
   @west {-1, 0}
 
-  @obstacle "#"
-  @space "."
-  @start "^"
+  @obstacle_tile "#"
+  @empty_tile "."
+  @start_tile "^"
 
   def part1(input_file) do
     input_file
     |> read_lines()
     |> build_grid()
-    |> process_grid()
+    |> calculate_route_length()
   end
 
   defp read_lines(input_file), do: File.read!(input_file ) |> String.split("\n", trim: true)
 
   defp build_grid(lines) do
     Enum.with_index(lines)
-    |> Enum.flat_map(&parse_row/1)
+    |> Enum.flat_map(&parse_grid_line/1)
     |> Map.new()
   end
 
-  defp parse_row({line, y}) do
+  defp parse_grid_line({line, y}) do
     line
     |> String.graphemes()
     |> Enum.with_index()
     |> Enum.map(fn {char, x} -> {{x, y}, char} end)
   end
 
-  defp process_grid(grid) do
+  defp calculate_route_length(grid) do
     start_position = find_start_position(grid)
 
     grid
@@ -43,11 +43,11 @@ defmodule AdventOfCode.Day06 do
       nil ->
         visited
 
-      tile when tile in [@space, @start] ->
+      tile when tile in [@empty_tile, @start_tile] ->
         new_visited = MapSet.put(visited, position)
         walk_grid(grid, step_forward(position, direction), direction, new_visited)
 
-      @obstacle ->
+      @obstacle_tile ->
         last_position = step_back(position, direction)
         new_direction = turn_right(direction)
 
@@ -55,7 +55,7 @@ defmodule AdventOfCode.Day06 do
     end
   end
 
-  defp find_start_position(grid), do: Enum.find(grid, fn {_k, v} -> v == @start end) |> elem(0)
+  defp find_start_position(grid), do: Enum.find(grid, fn {_k, v} -> v == @start_tile end) |> elem(0)
 
   defp step_forward({x, y}, {direction_x, direction_y}), do: {x + direction_x, y + direction_y}
 
@@ -74,6 +74,6 @@ defmodule AdventOfCode.Day06 do
     input_file
     |> read_lines()
     |> build_grid()
-    |> process_grid()
+    |> calculate_route_length()
   end
 end
