a76aa509c4d35e6a625f04c1d2ee885b3ffd13fd
Paulo Silva <github@p.silvahq.com>
2024-12-06 11:49:04 -0500

    rename map to grid

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -11,19 +11,13 @@ defmodule AdventOfCode.Day06 do
   def part1(input_file) do
     input_file
     |> read_lines()
-    |> build_map()
-    |> process_map()
+    |> build_grid()
+    |> process_grid()
   end
 
-  defp read_lines(input_file) do
-    input_file |> File.read!() |> String.split("\n", trim: true)
-  end
+  defp read_lines(input_file), do: File.read!(input_file ) |> String.split("\n", trim: true)
 
-  defp find_start_position(map) do
-    Enum.find(map, fn {_, v} -> v == @start end) |> elem(0)
-  end
-
-  defp build_map(lines) do
+  defp build_grid(lines) do
     Enum.with_index(lines)
     |> Enum.flat_map(&parse_row/1)
     |> Map.new()
@@ -36,6 +30,37 @@ defmodule AdventOfCode.Day06 do
     |> Enum.map(fn {char, x} -> {{x, y}, char} end)
   end
 
+  defp process_grid(grid) do
+    start_position = find_start_position(grid)
+
+    grid
+    |> walk_grid(start_position, @north)
+    |> Enum.count()
+  end
+
+  defp walk_grid(grid, position, direction, visited \\ MapSet.new()) do
+    case Map.get(grid, position) do
+      nil ->
+        visited
+
+      tile when tile in [@space, @start] ->
+        new_visited = MapSet.put(visited, position)
+        walk_grid(grid, step_forward(position, direction), direction, new_visited)
+
+      @obstacle ->
+        last_position = step_back(position, direction)
+        new_direction = turn_right(direction)
+
+        walk_grid(grid, step_forward(last_position, new_direction), new_direction, visited)
+    end
+  end
+
+  defp find_start_position(grid), do: Enum.find(grid, fn {_k, v} -> v == @start end) |> elem(0)
+
+  defp step_forward({x, y}, {direction_x, direction_y}), do: {x + direction_x, y + direction_y}
+
+  defp step_back({x, y}, {direction_x, direction_y}), do: {x - direction_x, y - direction_y}
+
   defp turn_right(direction) do
     case direction do
       @north -> @east
@@ -45,44 +70,10 @@ defmodule AdventOfCode.Day06 do
     end
   end
 
-  defp step_forward({x, y}, {direction_x, direction_y}) do
-    {x + direction_x, y + direction_y}
-  end
-
-  defp step_back({x, y}, {direction_x, direction_y}) do
-    {x - direction_x, y - direction_y}
-  end
-
-  defp process_map(map) do
-    start = find_start_position(map)
-
-    map
-    |> walk_map(start, @north)
-    |> Enum.count()
-  end
-
-  defp walk_map(map, position, direction, visited \\ MapSet.new()) do
-    case Map.get(map, position) do
-      nil ->
-        visited
-
-      tile when tile in [@space, @start] ->
-        new_visited = MapSet.put(visited, position)
-        walk_map(map, step_forward(position, direction), direction, new_visited)
-
-      @obstacle ->
-        last_position = step_back(position, direction)
-        new_direction = turn_right(direction)
-
-        walk_map(map, step_forward(last_position, new_direction), new_direction, visited)
-    end
-  end
-
   def part2(input_file) do
     input_file
     |> read_lines()
-    |> build_map()
-
-    0
+    |> build_grid()
+    |> process_grid()
   end
 end
