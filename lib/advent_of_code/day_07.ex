bfd04be1f6f2c99dca19928258162be3a6651347
Paulo Silva <github@p.silvahq.com>
2024-12-07 10:36:43 -0500

    Add day7 solution

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,45 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input_file) do
+    solve(input_file, [&Kernel.+/2, &Kernel.*/2])
   end
 
-  def part2(_args) do
+  def part2(input_file) do
+    solve(input_file, [&Kernel.+/2, &Kernel.*/2, &concat_numbers/2])
+  end
+
+  defp solve(input_file, operators) do
+    input_file
+    |> read_lines()
+    |> Enum.map(&parse_line/1)
+    |> Enum.filter(fn num -> valid_equation?(num, operators) end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  defp read_lines(input_file) do
+    File.read!(input_file) |> String.split("\n", trim: true)
+  end
+
+  defp parse_line(line) do
+    [result, numbers] = String.split(line, ":")
+
+    result = String.to_integer(result)
+    numbers = String.split(numbers) |> Enum.map(&String.to_integer/1)
+
+    {result, numbers}
+  end
+
+  defp valid_equation?({result, [first | rest]}, operators) do
+    do_math(result, rest, first, operators)
+  end
+
+  defp do_math(result, [], acc, _), do: result == acc
+
+  defp do_math(result, [num | rest], acc, operators) do
+    Enum.any?(operators, fn op -> do_math(result, rest, op.(acc, num), operators) end)
+  end
+
+  defp concat_numbers(num1, num2) do
+    (Integer.to_string(num1) <> Integer.to_string(num2)) |> String.to_integer()
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,31 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  test "part1 sample" do
+    input = "#{__DIR__}/fixtures/day7_sample.txt"
     result = part1(input)
 
-    assert result
+    assert result == 3749
   end
 
-  @tag :skip
+  test "part1 input" do
+    input = "#{__DIR__}/fixtures/day7_input.txt"
+    result = part1(input)
+
+    assert result == 5540634308362
+  end
+
   test "part2" do
-    input = nil
+    input = "#{__DIR__}/fixtures/day7_sample.txt"
     result = part2(input)
 
-    assert result
+    assert result == 11387
+  end
+
+  test "part2 input" do
+    input = "#{__DIR__}/fixtures/day7_input.txt"
+    result = part2(input)
+
+    assert result == 472290821152397
   end
 end
