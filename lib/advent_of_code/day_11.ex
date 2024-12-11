518345f5ae0a0372c8a0b03ae863dfda9ac0f9d2
Paulo Silva <github@p.silvahq.com>
2024-12-11 15:31:32 -0500

    refactor

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -9,28 +9,29 @@ defmodule AdventOfCode.Day11 do
     input
     |> parse_line_numbers()
     |> blink(times)
-    |> Enum.sum()
   end
 
   defp parse_line_numbers(line) do
-    line
-    |> String.split()
-    |> Enum.map(&String.to_integer/1)
+    String.split(line) |> Enum.map(&String.to_integer/1)
   end
 
   def blink(stones, n) do
-    Enum.map(stones, &(do_blink(&1, n, %{}) |> elem(0)))
+    Enum.reduce(stones, {0, %{}}, fn number, {total, cache} ->
+      {count, updated_cache} = do_blink(number, n, cache)
+      {total + count, updated_cache}
+    end)
+    |> elem(0)
   end
 
   def do_blink(_, 0, cache), do: {1, cache}
 
-  def do_blink(num, n, cache) do
-    cache_key = {num, n}
+  def do_blink(number, n, cache) do
+    cache_key = {number, n}
     cached_result = Map.get(cache, cache_key)
 
     if cached_result == nil do
-      {result, new_cache} =
-        case apply_rule(num) do
+      {result, updated_cache} =
+        case apply_rule(number) do
           [a, b] ->
             {result_a, cache_a} = do_blink(a, n - 1, cache)
             {result_b, cache_b} = do_blink(b, n - 1, cache_a)
@@ -40,7 +41,7 @@ defmodule AdventOfCode.Day11 do
             do_blink(a, n - 1, cache)
         end
 
-      {result, Map.put(new_cache, cache_key, result)}
+      {result, Map.put(updated_cache, cache_key, result)}
     else
       {cached_result, cache}
     end
@@ -48,22 +49,26 @@ defmodule AdventOfCode.Day11 do
 
   defp apply_rule(0), do: 1
 
-  defp apply_rule(num) do
-    if even_number_of_digits?(num) do
-      bisect_number(num)
+  defp apply_rule(number) do
+    if even_number_of_digits?(number) do
+      bisect_number(number)
     else
-      num * 2024
+      number * 2024
     end
   end
 
-  defp even_number_of_digits?(num), do: number_of_digits(num) |> Integer.is_even()
+  defp even_number_of_digits?(number) do
+    number_of_digits(number) |> Integer.is_even()
+  end
 
-  defp number_of_digits(num), do: Integer.digits(num) |> Enum.count()
+  defp number_of_digits(number) do
+    Integer.digits(number) |> Enum.count()
+  end
 
-  defp bisect_number(num) do
-    middle_index = number_of_digits(num) |> div(2)
+  defp bisect_number(number) do
+    middle_index = number_of_digits(number) |> div(2)
     divisor = :math.pow(10, middle_index) |> trunc
 
-    [div(num, divisor), rem(num, divisor)]
+    [div(number, divisor), rem(number, divisor)]
   end
 end
