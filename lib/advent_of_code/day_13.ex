3fcf55bba0d92cefb4634d54d94934248db4468e
Paulo Silva <github@p.silvahq.com>
2024-12-13 18:57:00 -0500

    Add day 13 solution

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,9 +1,76 @@
 defmodule AdventOfCode.Day13 do
   @moduledoc false
 
-  def part1(_args) do
+  @button_a_cost 3
+  @button_b_cost 1
+  @prize_offset 10_000_000_000_000
+
+  def part1(input_file) do
+    solve(input_file, 0)
   end
 
-  def part2(_args) do
+  def part2(input_file) do
+    solve(input_file, @prize_offset)
   end
+
+  def solve(input_file, offset) do
+    input_file
+    |> File.read!()
+    |> parse_input()
+    |> Enum.map(&add_prize_offset(&1, offset))
+    |> Enum.map(&solve/1)
+    |> Enum.sum()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&parse_machine/1)
+  end
+
+  defp parse_machine(line) do
+    [button_a, button_b, prize] =
+      line
+      |> String.split("\n")
+      |> Enum.map(&(String.split(&1, ": ") |> Enum.at(1)))
+
+    {parse_button(button_a), parse_button(button_b), parse_prize(prize)}
+  end
+
+  defp parse_button(button) do
+    button
+    |> String.split(", ")
+    |> Enum.map(&(String.slice(&1, 2..-1//1) |> String.to_integer()))
+    |> List.to_tuple()
+  end
+
+  defp parse_prize(prize) do
+    prize
+    |> String.split(", ")
+    |> Enum.map(&(String.split(&1, "=") |> Enum.at(1) |> String.to_integer()))
+    |> List.to_tuple()
+  end
+
+  defp add_prize_offset({button_a, button_b, {prize_x, prize_y}}, offset) do
+    {button_a, button_b, {prize_x + offset, prize_y + offset}}
+  end
+
+  def whole_number?(number) when is_float(number) do
+    number == Float.floor(number)
+  end
+
+  def solve({{a_x, a_y}, {b_x, b_y}, {prize_x, prize_y}}) do
+    count_a = (prize_x * b_y - prize_y * b_x) / (a_x * b_y - a_y * b_x)
+    count_b = (prize_x - a_x * count_a) / b_x
+
+    counts =
+      if whole_number?(count_a) and whole_number?(count_b) do
+        {count_a, count_b}
+      end
+
+    calculate_cost(counts)
+  end
+
+  defp calculate_cost(nil), do: 0
+  defp calculate_cost({count_a, count_b}), do: count_a * @button_a_cost + count_b * @button_b_cost
 end
--- test/advent_of_code/day_13_test.exs
+++ test/advent_of_code/day_13_test.exs
@@ -3,19 +3,24 @@ defmodule AdventOfCode.Day13Test do
 
   import AdventOfCode.Day13
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  test "part1 sample" do
+    input = "#{__DIR__}/fixtures/day13_sample.txt"
     result = part1(input)
 
-    assert result
+    assert result == 480
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
+  test "part1 input" do
+    input = "#{__DIR__}/fixtures/day13_input.txt"
+    result = part1(input)
+
+    assert result == 28_262
+  end
+
+  test "part2 input" do
+    input = "#{__DIR__}/fixtures/day13_input.txt"
     result = part2(input)
 
-    assert result
+    assert result == 101_406_661_266_314
   end
 end
