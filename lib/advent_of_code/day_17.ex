6530c0bf7d0c93f8c4fcc133978ed65920248512
Paulo Silva <github@p.silvahq.com>
2024-12-20 13:24:28 -0500

    Add day 17 part 1 solution

--- lib/advent_of_code/day_17.ex
+++ lib/advent_of_code/day_17.ex
@@ -1,7 +1,97 @@
+defmodule CPU do
+  @moduledoc false
+
+  defstruct register_a: 0,
+            register_b: 0,
+            register_c: 0,
+            instruction_pointer: 0,
+            instructions: [],
+            output: []
+end
+
 defmodule AdventOfCode.Day17 do
   @moduledoc false
 
-  def part1(_args) do
+  @adv 0
+  @bxl 1
+  @bst 2
+  @jnz 3
+  @bxc 4
+  @out 5
+  @bdv 6
+  @cdv 7
+
+  import Bitwise
+
+  def part1(cpu) do
+    simulate(cpu)
+  end
+
+  def simulate(
+        %CPU{
+          register_a: a,
+          register_b: b,
+          register_c: c,
+          instruction_pointer: ip,
+          instructions: instructions,
+          output: out
+        } = cpu
+      ) do
+    current_instruction = Enum.at(instructions, ip)
+    current_operand = Enum.at(instructions, ip + 1)
+
+    if current_instruction do
+      new_cpu =
+        case current_instruction do
+          @adv ->
+            op = combo_operand(cpu, current_operand)
+            %{cpu | register_a: div(a, 2 ** op), instruction_pointer: ip + 2}
+
+          @bxl ->
+            %{cpu | register_b: bxor(b, current_operand), instruction_pointer: ip + 2}
+
+          @bst ->
+            op = combo_operand(cpu, current_operand)
+            %{cpu | register_b: rem(op, 8), instruction_pointer: ip + 2}
+
+          @jnz ->
+            jnz(cpu, current_operand)
+
+          @bxc ->
+            %{cpu | register_b: bxor(b, c), instruction_pointer: ip + 2}
+
+          @out ->
+            op = combo_operand(cpu, current_operand)
+            %{cpu | output: [rem(op, 8) | out], instruction_pointer: ip + 2}
+
+          @bdv ->
+            op = combo_operand(cpu, current_operand)
+            %{cpu | register_b: div(a, 2 ** op), instruction_pointer: ip + 2}
+
+          @cdv ->
+            op = combo_operand(cpu, current_operand)
+            %{cpu | register_c: div(a, 2 ** op), instruction_pointer: ip + 2}
+        end
+
+      simulate(new_cpu)
+    else
+      Enum.reverse(out)
+    end
+  end
+
+  defp jnz(%CPU{register_a: a, instruction_pointer: ip} = cpu, op) do
+    new_ip = if(a == 0, do: ip + 2, else: op)
+    %{cpu | instruction_pointer: new_ip}
+  end
+
+  def combo_operand(%CPU{register_a: a, register_b: b, register_c: c}, operand) do
+    case operand do
+      op when op in 0..3 -> op
+      4 -> a
+      5 -> b
+      6 -> c
+      7 -> raise "combo operand 7 is invalid!"
+    end
   end
 
   def part2(_args) do
--- test/advent_of_code/day_17_test.exs
+++ test/advent_of_code/day_17_test.exs
@@ -3,12 +3,26 @@ defmodule AdventOfCode.Day17Test do
 
   import AdventOfCode.Day17
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "part1 sample" do
+    cpu = %CPU{
+      register_a: 729,
+      instructions: [0, 1, 5, 4, 3, 0]
+    }
 
-    assert result
+    result = part1(cpu)
+
+    assert result == [4, 6, 3, 5, 6, 3, 5, 2, 1, 0]
+  end
+
+  test "part1 input" do
+    cpu = %CPU{
+      register_a: 64_584_136,
+      instructions: [2, 4, 1, 2, 7, 5, 1, 3, 4, 3, 5, 5, 0, 3, 3, 0]
+    }
+
+    result = part1(cpu)
+
+    assert result == [3, 7, 1, 7, 2, 1, 0, 6, 3]
   end
 
   @tag :skip
