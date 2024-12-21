e8bb23f71df9ffa148957d0bcae489d284e9083e
Paulo Silva <github@p.silvahq.com>
2024-12-21 11:45:46 -0500

    more tweaks for day 17

--- lib/advent_of_code/day_17.ex
+++ lib/advent_of_code/day_17.ex
@@ -1,13 +1,7 @@
 defmodule CPU do
   @moduledoc false
 
-  defstruct register_a: 0,
-            register_b: 0,
-            register_c: 0,
-            ip: 0,
-            instructions: [],
-            output: [],
-            state: :running
+  defstruct a: 0, b: 0, c: 0, ip: 0, instructions: [], output: []
 end
 
 defmodule AdventOfCode.Day17 do
@@ -28,12 +22,12 @@ defmodule AdventOfCode.Day17 do
   def solve(_, [], _), do: nil
 
   def solve(
-        %CPU{register_a: a} = cpu,
+        %CPU{a: a} = cpu,
         [current_guess | remaining_guesses],
         [current_instruction | remaining_instructions] = instructions
       ) do
     # shift a 3 bits left and add the current_guess to the end
-    candidate_cpu = %{cpu | register_a: a * 8 + current_guess}
+    candidate_cpu = %{cpu | a: a * 8 + current_guess}
 
     case run_and_check(candidate_cpu) do
       %CPU{output: [^current_instruction]} ->
@@ -47,12 +41,14 @@ defmodule AdventOfCode.Day17 do
 
   def get_cpu_output(%CPU{} = cpu), do: Enum.reverse(cpu.output)
 
-  def run_and_check(%CPU{state: :halted} = cpu), do: cpu
+  # CPU halted
+  def run_and_check(%CPU{ip: nil} = cpu), do: cpu
   # Break on first output
   def run_and_check(%CPU{output: [_]} = cpu), do: cpu
   def run_and_check(%CPU{} = cpu), do: cpu |> step() |> run_and_check()
 
-  def run(%CPU{state: :halted} = cpu), do: cpu
+  # CPU halted
+  def run(%CPU{ip: nil} = cpu), do: cpu
   def run(%CPU{} = cpu), do: cpu |> step() |> run()
 
   def step(%CPU{} = cpu) do
@@ -66,31 +62,29 @@ defmodule AdventOfCode.Day17 do
     Enum.slice(instructions, ip, @instruction_size)
   end
 
-  def execute_instruction(cpu, [0, operand]), do: divide_a(cpu, operand, :register_a)
+  def execute_instruction(cpu, [0, operand]), do: divide_a(cpu, operand, :a)
   def execute_instruction(cpu, [1, operand]), do: xor_b_and_operand(cpu, operand)
   def execute_instruction(cpu, [2, operand]), do: b_mod_operand(cpu, operand)
   def execute_instruction(cpu, [3, operand]), do: jump_if_not_zero(cpu, operand)
   def execute_instruction(cpu, [4, _operand]), do: xor_b_and_c(cpu)
   def execute_instruction(cpu, [5, operand]), do: output(cpu, operand)
-  def execute_instruction(cpu, [6, operand]), do: divide_a(cpu, operand, :register_b)
-  def execute_instruction(cpu, [7, operand]), do: divide_a(cpu, operand, :register_c)
-  def execute_instruction(cpu, _instruction_and_operand), do: halt(cpu)
+  def execute_instruction(cpu, [6, operand]), do: divide_a(cpu, operand, :b)
+  def execute_instruction(cpu, [7, operand]), do: divide_a(cpu, operand, :c)
+  def execute_instruction(cpu, _instruction_and_operand), do: jump_to(cpu, nil)
 
-  defp halt(cpu), do: %{cpu | state: :halted}
-
-  defp xor_b_and_c(%CPU{register_b: b, register_c: c} = cpu) do
-    %{cpu | register_b: Bitwise.bxor(b, c)}
+  defp xor_b_and_c(%CPU{b: b, c: c} = cpu) do
+    %{cpu | b: Bitwise.bxor(b, c)}
   end
 
-  defp xor_b_and_operand(%CPU{register_b: b} = cpu, operand) do
-    %{cpu | register_b: Bitwise.bxor(b, operand)}
+  defp xor_b_and_operand(%CPU{b: b} = cpu, operand) do
+    %{cpu | b: Bitwise.bxor(b, operand)}
   end
 
   defp b_mod_operand(cpu, operand) do
-    %{cpu | register_b: rem(combo_operand(cpu, operand), 8)}
+    %{cpu | b: rem(combo_operand(cpu, operand), 8)}
   end
 
-  defp divide_a(%CPU{register_a: a} = cpu, operand, target_register) do
+  defp divide_a(%CPU{a: a} = cpu, operand, target_register) do
     %{cpu | target_register => div(a, 2 ** combo_operand(cpu, operand))}
   end
 
@@ -99,7 +93,7 @@ defmodule AdventOfCode.Day17 do
   end
 
   # No-op
-  defp jump_if_not_zero(%CPU{register_a: 0} = cpu, _operand), do: cpu
+  defp jump_if_not_zero(%CPU{a: 0} = cpu, _operand), do: cpu
   # Adjust operand to offset automatic instruction pointer incrementing
   defp jump_if_not_zero(%CPU{} = cpu, operand), do: jump_to(cpu, operand - @instruction_size)
 
@@ -108,8 +102,8 @@ defmodule AdventOfCode.Day17 do
   defp jump_to(cpu, index), do: %{cpu | ip: index}
 
   def combo_operand(_cpu, operand) when operand in 0..3, do: operand
-  def combo_operand(%CPU{register_a: a}, 4), do: a
-  def combo_operand(%CPU{register_b: b}, 5), do: b
-  def combo_operand(%CPU{register_c: c}, 6), do: c
+  def combo_operand(%CPU{a: a}, 4), do: a
+  def combo_operand(%CPU{b: b}, 5), do: b
+  def combo_operand(%CPU{c: c}, 6), do: c
   def combo_operand(_cpu, operand), do: raise("invalid combo operand '#{operand}'")
 end
--- test/advent_of_code/day_17_test.exs
+++ test/advent_of_code/day_17_test.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode.Day17Test do
 
   test "part1 sample" do
     cpu = %CPU{
-      register_a: 729,
+      a: 729,
       instructions: [0, 1, 5, 4, 3, 0]
     }
 
@@ -16,7 +16,7 @@ defmodule AdventOfCode.Day17Test do
 
   test "part1 input" do
     cpu = %CPU{
-      register_a: 64_584_136,
+      a: 64_584_136,
       instructions: [2, 4, 1, 2, 7, 5, 1, 3, 4, 3, 5, 5, 0, 3, 3, 0]
     }
 
@@ -28,12 +28,12 @@ defmodule AdventOfCode.Day17Test do
   test "part2 sample" do
     result = part2([0, 3, 5, 4, 3, 0])
 
-    assert result.register_a == 117_440
+    assert result.a == 117_440
   end
 
   test "part2 input" do
     result = part2([2, 4, 1, 2, 7, 5, 1, 3, 4, 3, 5, 5, 0, 3, 3, 0])
 
-    assert result.register_a == 37_221_334_433_268
+    assert result.a == 37_221_334_433_268
   end
 end
